701145f5615b82c1899e8a47f76ddbc8dcf31d08
Homayoun <homayoun763@gmail.com>
2026-09-18 20:53:08 +0330

    add : iransans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,45 @@
-import {Head} from 'nextra/components'
-import 'nextra-theme-blog/style.css'
-import '@/styles/globals.css'
 import CustomFooter from "@/components/custom-footer";
 import CustomHeader from "@/components/custom-header";
-import {Metadata} from "next";
-import {Layout} from "nextra-theme-blog";
-import {Inter} from 'next/font/google';
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import localFont from "next/font/local";
+import { Layout } from "nextra-theme-blog";
+import "nextra-theme-blog/style.css";
+import { Head } from "nextra/components";
 
 export const metadata: Metadata = {
-    title: 'itshomayoun'
-}
+  title: "itshomayoun",
+};
 
-const bodyFont = Inter({
-    subsets: ['latin', 'vietnamese'],
-})
+const bodyFont = localFont({
+  src: [
+    {
+      path: "../../public/IRANSansX.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
 
-export default async function RootLayout({children}) {
-    return (
-        <html
-            lang="fa"
-            dir="rtl"
-            suppressHydrationWarning
-
-            className={bodyFont.className}
-        >
-        <Head backgroundColor={{dark: '#15120d', light: '#faf5e9'}}/>
-        <body className="min-h-screen">
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      lang="fa"
+      dir="rtl"
+      suppressHydrationWarning
+      className={bodyFont.className}
+    >
+      <Head backgroundColor={{ dark: "#15120d", light: "#faf5e9" }} />
+      <body className="min-h-screen">
         <Layout>
-            <CustomHeader/>
+          <CustomHeader />
 
-            {children}
+          {children}
 
-            <CustomFooter/>
+          <CustomFooter />
         </Layout>
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
